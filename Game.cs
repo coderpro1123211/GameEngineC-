f2cc6351c274b733cf801943c1f525edbf3fc2d4
coderpro1123211 <osktor0325@utb.partille.se>
2017-07-09 09:42:44 +0200

    lel

--- Game.cs
+++ Game.cs
@@ -50,8 +50,8 @@ namespace GameEngine
             types[0] = typeof(Player);
             types[1] = typeof(Wall);
             SceneManager.AddScene(new Scene());
-            MapManager.loadMapFromFile(@"C:\Users\Alexander\Source\Repos\GameEngineC-2\Maps\map.txt", types);
-            MapManager.loadMapFromFile(@"C:\Users\Alexander\Source\Repos\GameEngineC-2\Maps\square.txt", types);
+            MapManager.loadMapFromFile(@"map.txt", types);
+            MapManager.loadMapFromFile(@"square.txt", types);
             SceneManager.LoadMap(1, 0, -7);
             SceneManager.LoadMap(0, -3, 0);
             keys = new List<ConsoleKeyInfo>();
